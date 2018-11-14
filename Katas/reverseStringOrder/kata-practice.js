df1f807f47d92d107c69edd31fa719c6ef2428b1
nictengkk <nictengkk@gmail.com>
2018-11-14 21:19:21 +0800

    completed refactoring

--- Katas/reverseStringOrder/kata-practice.js
+++ Katas/reverseStringOrder/kata-practice.js
@@ -3,17 +3,29 @@ let stringg = "i love to shit";
 
 function spinWords(str) {
   let array = str.split(" ");
+  //   let newArray = array.map(element => {
+  //     if (element.length >= 5) {
+  //       return element
+  //         .split("")
+  //         .reverse()
+  //         .join("");
+  //     } else {
+  //       return element;
+  //     }
+  //   });
+  //   return newArray.join(" ");
+  // }
+
   let newArray = array.map(element => {
-    if (element.length >= 5) {
-      return element
-        .split("")
-        .reverse()
-        .join("");
-    } else {
-      return element;
-    }
+    return element.length >= 5
+      ? element
+          .split("")
+          .reverse()
+          .join("")
+      : element;
   });
+
   return newArray.join(" ");
 }
 
-console.log(spinWords(stringg));
+console.log(spinWords(string));
